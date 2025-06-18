7b9f39a4fc2480ca140ccedd7e0b2b20c11dc280
PeregrineICT <peregrineict@gmail.com>
2025-06-18 12:27:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Manually set CORS headers (for full control)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',*);
+  res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
